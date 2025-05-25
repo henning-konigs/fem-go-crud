25d520b18c07e4006f27d02ffa4dada4a83e7441
hennong <henning.koenigs@gmx.de>
2025-05-25 10:36:55 +0200

    Create a HTTP server

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/henning-konigs/fem-go-crud/internal/app"
 )
 
@@ -12,4 +16,22 @@ func main() {
 	}
 
 	app.Logger.Println("We are running our app")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
